b1c1f1db25685354b89934a543f4f16fd5b8932f
cster143 <cster143@gmail.com>
2018-06-21 13:27:05 +0000

    Done.

---
+++ until.rb
@@ -0,0 +1,11 @@
+#Then, we'll solve this again by using an until loop. It will puts the phrase "Wingardium Leviosa"
+#until the levitation force is equal to 10, incrementing the levitation force by 1 each time we puts the phrase.
+
+def using_until
+levitation_force= 6
+until levitation_force  == 10 do
+  puts "Wingardium Leviosa"
+  levitation_force += 1
+  end
+end
+using_until
