1ece00efe23bbf1de45a264bfca759ecbb0b02f5
AleshaRP <createlele@yahoo.com>
2018-06-22 13:15:23 +0000

    Done.

--- until.rb
+++
@@ -1,8 +0,0 @@
-def using_until
-  force = 6
-  until force == 10
-    puts "Wingardium Leviosa"
-    force += 1
-  end
-end
-using_until
--- while.rb
+++
@@ -1,8 +0,0 @@
-def using_while
-  force = 6
-  while force < 10
-    puts "Wingardium Leviosa"
-    force += 1
-  end
-end
-using_while
